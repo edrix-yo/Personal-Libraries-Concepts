5951f93704ddfefc975256562773e3378c88d08d
edric yo <edricoal@gmail.com>
2024-11-06 22:14:54 +0700

    1.00 : adding logic for divisor but also implements interface

---
+++ Divisor_Sum_with_Interface/Program.cs
@@ -0,0 +1,33 @@
+﻿
+process divisor = new process();
+int resultOfDivisor = divisor.divisorSum(10);
+
+Console.WriteLine(resultOfDivisor);
+
+Console.WriteLine();
+
+
+
+
+public interface AdvancedArithmetic
+{
+    int divisorSum(int n);
+}
+
+public class process : AdvancedArithmetic
+{
+    public int divisorSum(int n)
+    {
+        int result = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                result += i;
+            }
+        }
+
+        return result;
+    }
+}
