861131677a68a9a94b6dd6cedf6f5155370fd58f
edric yo <edricoal@gmail.com>
2024-11-06 22:24:33 +0700

    1.01 : adding logic for store and retrieve list value inside dictionary

---
+++ List_Inside_Dictionary/Program.cs
@@ -0,0 +1,33 @@
+﻿
+Dictionary<(int, int), List<int>> abc = new Dictionary<(int, int), List<int>>();
+
+string[] testString = { "1 2 5", "1 3 4", "1 2 7" };
+
+var n = 0;
+var m = 0;
+var t = 0;
+
+foreach (var test in testString)
+{
+    n = int.Parse(test.Split()[0]);
+    m = int.Parse(test.Split()[1]);
+    t = int.Parse(test.Split()[2]);
+
+    if (abc.ContainsKey((n, m)))
+    {
+        abc[(n, m)].Add(t);
+    }
+    else
+    {
+        abc.Add((n, m), new List<int> { t });
+    }
+}
+
+foreach (var testDict in abc)
+{
+    Console.WriteLine("key" + testDict.Key);
+    foreach (var lsData in testDict.Value)
+    {
+        Console.WriteLine("valueDict : " + lsData);
+    }
+}
