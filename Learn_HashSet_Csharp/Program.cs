9ada5e53eecfd17c8adc543f742adb8c1496c347
edric yo <edricoal@gmail.com>
2024-11-08 23:53:31 +0700

    1.02 : adding simple logic for HashSet

---
+++ Learn_HashSet_Csharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+
+
+
+
+
+
+// inquiry a List
+List<int> abc = new List<int>();
+
+for (int i = 0; i < 20; i++)
+{
+    abc.Add(Random.Shared.Next(0, 10));
+}
+
+// start to move it into HashSet
+HashSet<int> resultDistinct = new HashSet<int>();
+
+foreach (int i in abc)
+{
+    resultDistinct.Add(i);
+}
+
+// to check the data, the result may not have duplicate number
+Console.Write(string.Join(",", resultDistinct));
+
+
+Task.Delay(1000);
